2b52230a0a12de526b42afe30ebe06c8960dae69
ShibuyaKosuke <github@shibuyakosuke.com>
2023-06-11 00:04:54 +0900

    basic setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('welcome');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', \App\Http\Controllers\DashboardController::class)->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile . edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile . update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile . destroy');
 });
+
+require __DIR__.'/auth.php';
